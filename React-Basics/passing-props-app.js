e96ae977b30a1c9928c66243b1df6f7068bf1a58
Joshua Conley <97537360+jpcc0@users.noreply.github.com>
2022-12-10 14:05:26 -0500

    Create passing-props-app.js

---
+++ React-Basics/passing-props-app.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="Bob" />
+      <Heading firstName="Any name other than Bob" />
+    </div>
+  );
+}
+
+export default App;
