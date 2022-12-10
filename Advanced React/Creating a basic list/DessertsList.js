44bad27b9cc49b1b214a2b8f1aa7f84a1906407b
Joshua Conley <97537360+jpcc0@users.noreply.github.com>
2022-12-10 15:42:21 -0500

    Create DessertsList.js

---
+++ Advanced React/Creating a basic list/DessertsList.js
@@ -0,0 +1,19 @@
+const DessertsList = (props) => {
+ const lowCaloriesDesserts = props.data
+   .filter((dessert) => {
+     return dessert.calories < 500;
+   })
+   .sort((a, b) => { 
+     return a.calories - b.calories; 
+   })
+   .map((dessert) => { 
+     return ( 
+       <li>
+         {dessert.name} - {dessert.calories} cal 
+       </li> 
+     ); 
+   }); 
+ return <ul>{lowCaloriesDesserts}</ul>; 
+
+}
+export default DessertsList; 
